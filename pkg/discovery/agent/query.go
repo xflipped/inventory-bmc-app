7d4dd4370cdb34107fe240f86f2d75550a4fa461
danil_e71 <danile71@gmail.com>
2023-02-07 18:58:42 +0300

    fix: rewrite discovery to agent

--- pkg/discovery/agent/query.go
+++ pkg/discovery/agent/query.go
@@ -9,8 +9,9 @@ import (
 	"git.fg-tech.ru/listware/cmdb/pkg/cmdb/qdsl"
 )
 
-func (a *Agent) getDocument(query string) (document *documents.Node, err error) {
-	documents, err := qdsl.Qdsl(a.ctx, query, qdsl.WithKey(), qdsl.WithId())
+func (a *Agent) getDocument(format string, args ...any) (document *documents.Node, err error) {
+	query := fmt.Sprintf(format, args...)
+	documents, err := qdsl.Qdsl(a.ctx, query, qdsl.WithKey(), qdsl.WithId(), qdsl.WithType())
 	if err != nil {
 		return
 	}
