f9fb1430c787960c987b3d3da3d98c3ae9c38a61
Lesia Miliutina <alesia.miliutsina@gmail.com>
2023-03-09 15:55:04 +0300

    fix(ci): fix golangci job

--- pkg/led/bootstrap/types.go
+++ pkg/led/bootstrap/types.go
@@ -4,11 +4,7 @@ package bootstrap
 
 import (
 	"context"
-	"fmt"
 
-	"git.fg-tech.ru/listware/cmdb/pkg/cmdb/qdsl"
-	"git.fg-tech.ru/listware/cmdb/pkg/cmdb/vertex/types"
-	"git.fg-tech.ru/listware/go-core/pkg/client/system"
 	"git.fg-tech.ru/listware/proto/sdk/pbcmdb"
 )
 
@@ -16,26 +12,26 @@ var (
 	registerTypes = []*pbcmdb.RegisterTypeMessage{}
 )
 
-func createType(ctx context.Context, pt *types.Type) (err error) {
-	query := fmt.Sprintf("%s.types.root", pt.Schema.Title)
-	elements, err := qdsl.Qdsl(ctx, query)
-	if err != nil {
-		return
-	}
+// func createType(ctx context.Context, pt *types.Type) (err error) {
+// 	query := fmt.Sprintf("%s.types.root", pt.Schema.Title)
+// 	elements, err := qdsl.Qdsl(ctx, query)
+// 	if err != nil {
+// 		return
+// 	}
 
-	// TODO already exists
-	if len(elements) > 0 {
-		return
-	}
+// 	// TODO already exists
+// 	if len(elements) > 0 {
+// 		return
+// 	}
 
-	message, err := system.RegisterType(pt, true)
-	if err != nil {
-		return
-	}
+// 	message, err := system.RegisterType(pt, true)
+// 	if err != nil {
+// 		return
+// 	}
 
-	registerTypes = append(registerTypes, message)
-	return
-}
+// 	registerTypes = append(registerTypes, message)
+// 	return
+// }
 
 func createTypes(ctx context.Context) (err error) {
 	// new types are not required
